71b9d319da9ad3611c9f3b8d7d2e1d826652fc04
Mo <moritz.boullenger@outlook.com>
2022-06-07 21:58:09 +0300

    styling and javascript animation

--- asciiRunners/forms.py
+++ asciiRunners/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class RunnerForm(forms.Form):
-    runner_id = forms.CharField(label='Runner ID', max_length=5)
-    contrast_mode = forms.CharField(label='Contrast Mode', max_length=1)
-    character_type = forms.CharField(label='Character Type', max_length=20)
+    runner_id = forms.CharField(label='Runner ID', max_length=5, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    contrast_mode = forms.CharField(label='Contrast Mode', max_length=1, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    character_type = forms.CharField(label='Character Type', max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
